4887eedd6884ff04820a3dfaf10b72f9e226f96e
Mahokkit <ma.edward@hotmail.com>
2018-01-09 18:06:03 -0400

    First C# Commit!

---
+++ FirstSolutionForTesting/TestAppToPushGithub/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestAppToPushGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from the console!");
+
+            ConsoleKeyInfo myFavoriteKey = Console.ReadKey(true);
+
+            Console.WriteLine("My fav key is " + myFavoriteKey.Key);
+
+            Console.ReadKey();
+        }
+    }
+}
