3b2c6675e7193d12f49d901dc55c5da4a3d66da7
Mahokkit <ma.edward@hotmail.com>
2018-01-10 15:08:32 -0400

    First commit

---
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //recognize when a specific key is pressed
+
+            //read or take in input
+            string message = Console.ReadLine();
+
+            //display output
+            Console.WriteLine("Here's some output!!");
+            Console.WriteLine("Here's your message: {0}", message);
+                     
+           //recognize flag at startup (-server)
+
+            //keep window open
+            Console.ReadKey();
+        }
+    }
+}
