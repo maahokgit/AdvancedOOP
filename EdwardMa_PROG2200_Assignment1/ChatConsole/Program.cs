5e0a1cd2200e60f414d835ed96c60715dcdd150e
Mahokkit <ma.edward@hotmail.com>
2018-01-10 19:30:43 -0400

    created library and inserted to program

--- EdwardMa_PROG2200_Assignment1/ChatConsole/Program.cs
+++ EdwardMa_PROG2200_Assignment1/ChatConsole/Program.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ChatLibrary;
 namespace ChatConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //check if in server mode or not
+            if(args.Contains("-server"))
+            {
+                //server mode
+                Server server = new Server();
+            }
+            else
+            {
+                //client mode
+                Client client = new Client();
+            }
         }
     }
 }
