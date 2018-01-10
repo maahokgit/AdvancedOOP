490de39530dacdf8dbce4ca70a1b9b4ff4011e1e
Mahokkit <ma.edward@hotmail.com>
2018-01-10 18:53:05 -0400

    save point

--- FirstConsoleApp/FirstConsoleApp/Program.cs
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FirstConsoleApp
@@ -10,16 +11,29 @@ namespace FirstConsoleApp
     {
         static void Main(string[] args)
         {
-            //recognize when a specific key is pressed
 
-            ConsoleKeyInfo pressedKey = Console.ReadKey(true);
-            if (pressedKey.Key == ConsoleKey.I)
+
+            //recognize when a specific key is pressed
+            while (true) //endless loop
             {
-                Console.WriteLine("You Pressed the I Key!!");
-            }
-            else
-            {
-                Console.WriteLine("You didn't press the I key...");
+                //check for messages
+                Console.WriteLine("Checking Messages");
+               
+                //check for key presses
+                if(Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+                    if (pressedKey.Key == ConsoleKey.I)
+                    {
+                        Console.WriteLine("You Pressed the I Key!!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You didn't press the I key...");
+                    }
+                }
+
+                Thread.Sleep(250);
             }
 
             //read or take in input
