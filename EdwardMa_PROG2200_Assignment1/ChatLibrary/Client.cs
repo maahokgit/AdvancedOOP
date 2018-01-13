481e75ff343cf1d9987d1824ee1fc0e513c04575
Mahokkit <ma.edward@hotmail.com>
2018-01-13 16:37:21 -0400

    insert pseudo code...

--- EdwardMa_PROG2200_Assignment1/ChatLibrary/Client.cs
+++ EdwardMa_PROG2200_Assignment1/ChatLibrary/Client.cs
@@ -8,5 +8,19 @@ namespace ChatLibrary
 {
     public class Client
     {
+        public void main()
+        {
+            while(true)
+            {
+                //check server for message
+                //if there's a message... display
+                //if user press 'I' then
+                    //let user sent message
+                    //if user enter 'quit'
+                        //quit or exit loop
+            }
+
+            //exit message, user press any key to quit
+        }
     }
 }
--- EdwardMa_PROG2200_Assignment1/ChatLibrary/Server.cs
+++ EdwardMa_PROG2200_Assignment1/ChatLibrary/Server.cs
@@ -8,5 +8,20 @@ namespace ChatLibrary
 {
     public class Server
     {
+        public void main()
+        {
+            //start server
+            while(true)
+            {
+                //wait for client message
+                //display client message if there is one
+                //if user press 'I' then
+                    //let user sent message
+                    //if user enter 'quit'
+                    //quit or exit loop
+            }
+
+            //exit message, user press any key to quit
+        }
     }
 }
