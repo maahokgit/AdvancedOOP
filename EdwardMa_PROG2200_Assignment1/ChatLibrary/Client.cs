35a5bc851f7ed01f74ed51133e86a40d03570383
Mahokkit <ma.edward@hotmail.com>
2018-01-14 13:20:37 -0400

    moving demo class to library class

--- EdwardMa_PROG2200_Assignment1/ChatLibrary/Client.cs
+++ EdwardMa_PROG2200_Assignment1/ChatLibrary/Client.cs
@@ -3,24 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
 
 namespace ChatLibrary
 {
     public class Client
     {
-        public void main()
+        static void Main(string[] args)
         {
-            while(true)
+            Connect("127.0.0.1", "HELLO!");
+        }
+
+        static void Connect(String server, String message)
+        {
+            try
             {
-                //check server for message
-                //if there's a message... display
-                //if user press 'I' then
-                    //let user sent message
-                    //if user enter 'quit'
-                        //quit or exit loop
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer 
+                // connected to the same address as specified by the server, port
+                // combination.
+                Int32 port = 13000;
+                TcpClient client = new TcpClient(server, port);
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                // Get a client stream for reading and writing.
+                //  Stream stream = client.GetStream();
+
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", message);
+
+                // Receive the TcpServer.response.
+
+                // Buffer to store the response bytes.
+                data = new Byte[256];
+
+                // String to store the response ASCII representation.
+                String responseData = String.Empty;
+
+                // Read the first batch of the TcpServer response bytes.
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
             }
 
-            //exit message, user press any key to quit
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
         }
     }
 }
